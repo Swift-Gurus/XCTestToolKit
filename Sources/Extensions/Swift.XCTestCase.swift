231b0ee81101e4fa28718a2f7762b9b44c8700d7
Alex Crowe <11133687+AlexHmelevski@users.noreply.github.com>
2023-12-27 14:33:38 -0500

    removed async

--- Sources/Extensions/Swift.XCTestCase.swift
+++ Sources/Extensions/Swift.XCTestCase.swift
@@ -18,7 +18,7 @@ public func XCTAssertThrowsErrorAsync(_ block: () async throws -> Void,
 public func XCTAssertThrowsError(_ block: () throws -> Void,
                                  catchBlock: ((Error) -> Void)? = nil,
                                  file: StaticString = #filePath,
-                                 line: UInt = #line) async {
+                                 line: UInt = #line) {
     do {
         try block()
         XCTFail("Expect to throw an error but was success", file: file, line: line)
