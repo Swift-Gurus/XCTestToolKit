1d762266207eeed063f1fcf69e21a2c9faa5411a
Alex Crowe <11133687+AlexHmelevski@users.noreply.github.com>
2023-12-27 14:31:38 -0500

    removed unused message add throw check function

--- Sources/Extensions/Swift.XCTestCase.swift
+++ Sources/Extensions/Swift.XCTestCase.swift
@@ -5,11 +5,22 @@ import XCTest
 
 public func XCTAssertThrowsErrorAsync(_ block: () async throws -> Void,
                                       catchBlock: ((Error) -> Void)? = nil,
-                                      _ message: @autoclosure () -> String = "",
                                       file: StaticString = #filePath,
                                       line: UInt = #line) async {
     do {
         try await block()
+        XCTFail("Expect to throw an error but was success", file: file, line: line)
+    } catch {
+        catchBlock?(error)
+    }
+}
+
+public func XCTAssertThrowsError(_ block: () throws -> Void,
+                                 catchBlock: ((Error) -> Void)? = nil,
+                                 file: StaticString = #filePath,
+                                 line: UInt = #line) async {
+    do {
+        try block()
         XCTFail("Expect to throw an error but was success", file: file, line: line)
     } catch {
         catchBlock?(error)
