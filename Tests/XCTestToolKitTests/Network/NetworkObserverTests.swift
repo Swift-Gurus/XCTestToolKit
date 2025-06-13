0a3bd8a99a31b606bd76de3ee6951325828b18b2
Alex Crowe <alexei.hmelevski@gmail.com>
2025-06-13 10:25:38 -0400

    - renaming (#2)
    - public access

--- Tests/XCTestToolKitTests/Network/NetworkObserverTests.swift
+++ Tests/XCTestToolKitTests/Network/NetworkObserverTests.swift
@@ -4,53 +4,49 @@ import Testing
 @Suite(.serialized)
 struct NetworkObserverTests {
     let stressTestCount = 1000
-
+    var networkMonitor = NetworkTestMonitor()
     @Test func simple() async throws {
-        let observer = NetworkObserver()
-        _ = try await observer.mockSession.data(for: .init(url: .mockURL))
+        _ = try await networkMonitor.mockSession.data(for: .init(url: .mockURL))
     }
 
     @Test
     func test_waits_for_all_main_requests_and_returns_their_values() async throws {
-        let observer = NetworkObserver()
-        observer.setObserveRequests(stressTestCount)
+        networkMonitor.setObserveRequests(stressTestCount)
 
         try await asyncStress { _ in
-            try await observer.mockSession.data(for: .init(url: .mockURL))
+            try await networkMonitor.mockSession.data(for: .init(url: .mockURL))
         }
 
-        let request = try await observer.allRequests
+        let request = try await networkMonitor.allRequests
         #expect(request.count == stressTestCount)
 
     }
 
     @Test
     func test_waits_for_all_secondary_requests_and_returns_their_values() async throws {
-        let observer = NetworkObserver()
-        observer.setObserveSecondaryRequests(stressTestCount)
+        networkMonitor.setObserveSecondaryRequests(stressTestCount)
 
         try await asyncStress { _ in
-            try await observer.secondarySession.data(for: .init(url: .mockURL))
+            try await networkMonitor.secondarySession.data(for: .init(url: .mockURL))
         }
 
-        let request = try await observer.allSecondaryRequestURLs
+        let request = try await networkMonitor.allSecondaryRequestURLs
         #expect(request.count == stressTestCount)
 
     }
 
     @Test
     func test_waits_for_all_requests_and_returns_their_values() async throws {
-        let observer = NetworkObserver()
-        observer.setObserveSecondaryRequests(stressTestCount)
-        observer.setObserveRequests(stressTestCount)
+        networkMonitor.setObserveSecondaryRequests(stressTestCount)
+        networkMonitor.setObserveRequests(stressTestCount)
 
         try await asyncStress { _ in
-            _ = try await observer.secondarySession.data(for: .init(url: .mockURL))
-            _ = try await observer.mockSession.data(for: .init(url: .mockURL))
+            _ = try await networkMonitor.secondarySession.data(for: .init(url: .mockURL))
+            _ = try await networkMonitor.mockSession.data(for: .init(url: .mockURL))
         }
 
-        let secRequest = try await observer.allSecondaryRequestURLs
-        let mainRequest = try await observer.allRequests
+        let secRequest = try await networkMonitor.allSecondaryRequestURLs
+        let mainRequest = try await networkMonitor.allRequests
         #expect(secRequest.count == stressTestCount)
         #expect(mainRequest.count == stressTestCount)
     }
